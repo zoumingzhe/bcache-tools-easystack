f41139b9e4845f8284c17680ab4b79826a9819f1
kcchuang <kc.chuang@easystack.cn>
2020-02-18 10:16:23 +0800

    Permanently load escache module for alcubierre dev
    Signed-off-by: kcchuang <kc.chuang@easystack.cn>

--- alcubierre-check.c
+++ alcubierre-check.c
@@ -11,6 +11,9 @@ int main(int argc, char **argv)
 	int disk_fd;
 	char buf[11] = {0};
 	ssize_t count;
+	char bdev_name[100] = {0};
+	char escache_dir[100] = {0};
+	int ret;
 
 	if(argc != 2) {
 		printf("Usage: alcubierre-check NODE\n");
@@ -35,6 +38,19 @@ int main(int argc, char **argv)
 	} else {
 		printf("ALCUBIERRE_DEV=no\n");
 	}
+
+	ret = sscanf(argv[1], "/dev/%s", bdev_name);
+	if (ret != 1) {
+		printf("Can not parse '/dev/bdev_name' from %s\n", argv[1]);
+		close(disk_fd);
+		return -1;
+	}
+
+	sprintf(escache_dir, "/sys/block/%s/escache/set", bdev_name);
+	if (access(escache_dir, F_OK) != -1)
+		printf("ALCUBIERRE_REGISTERED=yes\n");
+	else
+		printf("ALCUBIERRE_REGISTERED=no\n");
 
 	close(disk_fd);
 	return 0;
